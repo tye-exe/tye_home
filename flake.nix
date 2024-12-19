f43e1dd3112418a95eecad2cf415ef1665f56f70
tye-exe <tye@mailbox.org>
2024-12-19 14:10:06 +0000

    Updated insecure dependencies

--- flake.nix
+++ flake.nix
@@ -7,12 +7,22 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
-      in with pkgs; {
+      in
+      with pkgs;
+      {
         devShells.default = mkShell rec {
           buildInputs = [
             # Rust
@@ -45,5 +55,6 @@
 
           LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
         };
-      });
+      }
+    );
 }
